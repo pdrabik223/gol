7d4f1aa465ad00df013abc64e8c7f936fcfb5e13
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-06-13 16:55:24 +0200

    test3

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ int main() {
     x = 7; // game will be 7 cells high
     y = 7; // and 7 cells width
     game_engine life(x, y);
-    life.get_cell(2,3) = true;
-    life.get_cell(3,4) = true;
-    life.get_cell(4,4) = true;
-    life.get_cell(4,3) = true;
-    life.get_cell(4,2) = true;
+//    life.get_cell(2,3) = true;
+//    life.get_cell(3,4) = true;
+//    life.get_cell(4,4) = true;
+//    life.get_cell(4,3) = true;
+//    life.get_cell(4,2) = true;
 
 
 
